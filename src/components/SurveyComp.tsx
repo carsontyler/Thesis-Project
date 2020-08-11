d89bb23d04330265d2b6b2516c9c816a0e54b9a8
carsontyler <17ctyler@gmail.com>
2020-08-11 11:50:26 -0600

    Flow mostly working.

--- src/components/SurveyComp.tsx
+++ src/components/SurveyComp.tsx
@@ -1,16 +1,132 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StarRatingComponent from "react-star-rating-component";
 import { CardHeader, Card, CardTitle, CardBody } from "reactstrap";
 import { View, Button} from "react-native";
 import "./../views/HomePage/homepage.css"
+import { Recipe } from "../views/HomePage/HomePage";
+import _ from "lodash";
 
 export interface SurvepCompProps {
   submitSurvey: any;
+  recipe: Recipe;
+  similar_recipes: Recipe[];
 }
 
 export const SurveyComp : React.FC<SurvepCompProps> = (props) => {
+
+  let [rating1, setRating1] = useState(0);
+  let [rating2, setRating2] = useState(0);
+  let [rating3, setRating3] = useState(0);
+  let [rating4, setRating4] = useState(0);
+  let [rating5, setRating5] = useState(0);
+  let [rating6, setRating6] = useState(0);
+  let [rating7, setRating7] = useState(0);
+  let [rating8, setRating8] = useState(0);
+  let [rating9, setRating9] = useState(0);
+  let [rating10, setRating10] = useState(0);
+  let [rating11, setRating11] = useState(0);
+  let [rating12, setRating12] = useState(0);
+  let [rating13, setRating13] = useState(0);
+  let [rating14, setRating14] = useState(0);
+  let [rating15, setRating15] = useState(0);
+  let [rating16, setRating16] = useState(0);
+  let [rating17, setRating17] = useState(0);
+  let [rating18, setRating18] = useState(0);
+  let [rating19, setRating19] = useState(0);
+  let [rating20, setRating20] = useState(0);
+  let [rating21, setRating21] = useState(0);
+  let [rating22, setRating22] = useState(0);
+  let [rating23, setRating23] = useState(0);
+  let [rating24, setRating24] = useState(0);
+  let [rating25, setRating25] = useState(0);
+  let [rating26, setRating26] = useState(0);
+  let [rating27, setRating27] = useState(0);
+
+  const onStarclick1 = (event: any) => {
+    setRating1(event);
+  }
+  const onStarclick2 = (event: any) => {
+    setRating2(event);
+  }
+  const onStarclick3 = (event: any) => {
+    setRating3(event);
+  }
+  const onStarclick4 = (event: any) => {
+    setRating4(event);
+  }
+  const onStarclick5 = (event: any) => {
+    setRating5(event);
+  }
+  const onStarclick6 = (event: any) => {
+    setRating6(event);
+  }
+  const onStarclick7 = (event: any) => {
+    setRating7(event);
+  }
+  const onStarclick8 = (event: any) => {
+    setRating8(event);
+  }
+  const onStarclick9 = (event: any) => {
+    setRating9(event);
+  }
+  const onStarclick10 = (event: any) => {
+    setRating10(event);
+  }
+  const onStarclick11 = (event: any) => {
+    setRating11(event);
+  }
+  const onStarclick12 = (event: any) => {
+    setRating12(event);
+  }
+  const onStarclick13 = (event: any) => {
+    setRating13(event);
+  }
+  const onStarclick14 = (event: any) => {
+    setRating14(event);
+  }
+  const onStarclick15 = (event: any) => {
+    setRating15(event);
+  }
+  const onStarclick16 = (event: any) => {
+    setRating16(event);
+  }
+  const onStarclick17 = (event: any) => {
+    setRating17(event);
+  }
+  const onStarclick18 = (event: any) => {
+    setRating18(event);
+  }
+  const onStarclick19 = (event: any) => {
+    setRating19(event);
+  }
+  const onStarclick20 = (event: any) => {
+    setRating20(event);
+  }
+  const onStarclick21 = (event: any) => {
+    setRating21(event);
+  }
+  const onStarclick22 = (event: any) => {
+    setRating22(event);
+  }
+  const onStarclick23 = (event: any) => {
+    setRating23(event);
+  }
+  const onStarclick24 = (event: any) => {
+    setRating24(event);
+  }
+  const onStarclick25 = (event: any) => {
+    setRating25(event);
+  }
+  const onStarclick26 = (event: any) => {
+    setRating26(event);
+  }
+  const onStarclick27 = (event: any) => {
+    setRating27(event);
+  }
+  
+
   return (
-    <div>
+    <div style={{overflow: 'auto'}}>
       <Card className="card-chart">
         <CardHeader>
           <CardTitle tag="h3">
@@ -25,8 +141,9 @@ export const SurveyComp : React.FC<SurvepCompProps> = (props) => {
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
               <StarRatingComponent 
-                name="rating"
-                value={0}
+                name="rating1"
+                value={rating1}
+                onStarClick={onStarclick1}
               />
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>          
@@ -34,41 +151,246 @@ export const SurveyComp : React.FC<SurvepCompProps> = (props) => {
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
               <StarRatingComponent 
-                name="rating"
-                value={0}
+                name="rating2"
+                value={rating2}
+                onStarClick={onStarclick2}
               />
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
               <span>On a scale of 1-5, please rate each recommendation's accuracy to your intended selection:</span>
             </View>
+
+
             <View style={{flex: 1, flexDirection: 'row'}}>
-              <StarRatingComponent 
-                name="rating"
-                value={0}
-              />
+              <View style={{flex: 1, flexDirection: 'column'}}></View>
+              <View style={{flex: 9, flexDirection: 'column'}}>
+            
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[0].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating8"
+                    value={rating8}
+                    onStarClick={onStarclick8}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>        
+                  <span>{props.similar_recipes[1].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating9"
+                    value={rating9}
+                    onStarClick={onStarclick9}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[2].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating10"
+                    value={rating10}
+                    onStarClick={onStarclick10}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[3].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating11"
+                    value={rating11}
+                    onStarClick={onStarclick11}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[4].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating12"
+                    value={rating12}
+                    onStarClick={onStarclick12}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[5].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating13"
+                    value={rating13}
+                    onStarClick={onStarclick13}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[6].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating14"
+                    value={rating14} 
+                    onStarClick={onStarclick14}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[7].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating15"
+                    value={rating15} 
+                    onStarClick={onStarclick15}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[8].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating16"
+                    value={rating16} 
+                    onStarClick={onStarclick16}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[9].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating17"
+                    value={rating17} 
+                    onStarClick={onStarclick17}
+                  />
+                </View>
+
+              </View>
             </View>
-            <View style={{flex: 1, flexDirection: 'row'}}>
-              <span>LIST EACH RECOMMENDATION HERE</span>
-            </View>
+
+
             <View style={{flex: 1, flexDirection: 'row'}}>
               <span>On a scale of 1-5, how unexpected was each recommendation?</span>
             </View>
+
             <View style={{flex: 1, flexDirection: 'row'}}>
-              <StarRatingComponent 
-                name="rating"
-                value={0}
-              />
+              <View style={{flex: 1, flexDirection: 'column'}}></View>
+              <View style={{flex: 9, flexDirection: 'column'}}>
+
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[0].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating18"
+                    value={rating18}
+                    onStarClick={onStarclick18}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>        
+                  <span>{props.similar_recipes[1].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating19"
+                    value={rating19}
+                    onStarClick={onStarclick19}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[2].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating20"
+                    value={rating20}
+                    onStarClick={onStarclick20}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[3].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating21"
+                    value={rating21}
+                    onStarClick={onStarclick21}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[4].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating22"
+                    value={rating22}
+                    onStarClick={onStarclick22}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[5].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating23"
+                    value={rating23}
+                    onStarClick={onStarclick23}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[6].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating24"
+                    value={rating24} 
+                    onStarClick={onStarclick24}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[7].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating25"
+                    value={rating25} 
+                    onStarClick={onStarclick25}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[8].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating26"
+                    value={rating26} 
+                    onStarClick={onStarclick26}
+                  />
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <span>{props.similar_recipes[9].title}</span>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <StarRatingComponent 
+                    name="rating27"
+                    value={rating27} 
+                    onStarClick={onStarclick27}
+                  />
+                </View>
+
+              </View>
             </View>
-            <View style={{flex: 1, flexDirection: 'row'}}>
-              <span>LIST EACH RECOMMENDATION HERE</span>
-            </View>
+
+
             <View style={{flex: 1, flexDirection: 'row'}}>
               <span>On a scale of 1-5, how likely are you to reuse this recommendation system for future recipe recommendations?</span>
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
               <StarRatingComponent 
-                name="rating"
-                value={0}
+                name="rating5"
+                value={rating5}
+                onStarClick={onStarclick5}
               />
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
@@ -76,8 +398,9 @@ export const SurveyComp : React.FC<SurvepCompProps> = (props) => {
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
               <StarRatingComponent 
-                name="rating"
-                value={0}
+                name="rating6"
+                value={rating6}
+                onStarClick={onStarclick6}
               />
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
@@ -85,12 +408,15 @@ export const SurveyComp : React.FC<SurvepCompProps> = (props) => {
             </View>
             <View style={{flex: 1, flexDirection: 'row'}}>
               <StarRatingComponent 
-                name="rating"
-                value={0} 
+                name="rating7"
+                value={rating7} 
+                onStarClick={onStarclick7}
               />
             </View>
+
+
             <View style={{flex: 1, flexDirection: 'row'}}>
-              <Button onPress={props.submitSurvey} title="Select Recipe" color="#4ECCA3" /> 
+              <Button onPress={props.submitSurvey} title="Submit Survey" color="#4ECCA3" /> 
             </View>
           </View>
         </CardBody>
--- src/views/HomePage/HomePage.tsx
+++ src/views/HomePage/HomePage.tsx
@@ -1,10 +1,8 @@
 import React, { Fragment, useEffect, useState } from "react"
 import _ from 'lodash'
-// import { HomePageComp } from "../../components/HomePageComp"
 import { View, Button } from "react-native"
 import {Card, CardHeader, CardTitle, CardBody, Col, CardImg, Row} from "reactstrap"
 import { RecipeComp } from "./../../components/RecipeComp";
-import { RecipeRaterComp } from "./../../components/RecipeRaterComp"
 import { SurveyComp } from "../../components/SurveyComp"
 import "./homepage.css"
 
@@ -29,12 +27,17 @@ const side_recipes = require('./../../data/side_recipes.json')
 
 export const HomePage: React.FC = () => {  
   
-  const varToString = (varObj: {}) => Object.keys(varObj)[0]
-  const [allRecipes, setRecipes] = useState<Recipe[]>();
-  const [mainRecipes, setMainRecipes] = useState<Recipe[]>();
+  const [allRecipes, setRecipes] = useState<Recipe[]>([]);
+  const [mainRecipes, setMainRecipes] = useState<Recipe[]>([]);
   let [currentRecipe, setCurrentRecipe] = useState<Recipe>();
+  let [tempCurrentRecipe, setTempCurrentRecipe] = useState<Recipe>();
   let [groupId, setGroupId] = useState(0);
   let [index, setIndex] = useState(-1);
+  let [directionsAccepted, setDirectionsAccepted] = useState(false);
+  
+  let [similar_recipes, setSimilarRecipes] = useState<Recipe[]>([]);
+  let [certain_recipes, setCertainRecipes] = useState<Recipe[]>([]);
+  let [uncertain_recipes, setUncertainRecipes] = useState<Recipe[]>([]);
     
   interface Recipe {
     id: number,
@@ -49,20 +52,50 @@ export const HomePage: React.FC = () => {
     directions: [string],
   }
   
-  const handleClick = (rcp: any) => {
-    setCurrentRecipe(rcp);
+  const handleClick = (rcp: Recipe) => {
+    if (index === 1)
+      setTempCurrentRecipe(rcp);
+    else if (index === 0)
+      setCurrentRecipe(rcp);
+    if (rcp.most_similar.length > 0) {
+      setSimilarRecipes(_.filter(allRecipes, (rcp: Recipe) => {
+        return currentRecipe ? currentRecipe.most_similar.includes(rcp.id) : false;
+      }));
+      setCertainRecipes(_.filter(similar_recipes, (rcp: Recipe) => {
+        return rcp.type === 'certain';
+      }));
+      setUncertainRecipes(_.filter(similar_recipes, (rcp: Recipe) => {
+        return rcp.type === 'uncertain';
+      }));
+    }
+  }
+
+  const handleSubmit = (event: any) => {
+    setIndex(0);
   }
  
-  let selectRecipeClick = () => {
+  const selectRecipeClick = () => {
     let temp = ++index;
-    if (temp > 1){
+    if (temp === 1)
+      setTempCurrentRecipe(currentRecipe);
+    if (temp > 1) {
       temp = 0;
       setGroupId(++groupId);
+      if (groupId === 5)
+        temp = 2;
     }
     setIndex(temp);
   }
 
-  useEffect(() => {
+  const directionsCheckboxChange = (event : any) => {
+    setDirectionsAccepted(event.target.checked);
+  }
+
+  const submitSurvey = (event: any) => {
+    selectRecipeClick();
+  }
+
+  const refresh = () => {
     let recipes:Recipe[] = [];
 
     if (groupId === 0)
@@ -78,34 +111,39 @@ export const HomePage: React.FC = () => {
 
     setRecipes(recipes);
 
-    let mainRecipes = _.filter(dinner_recipes, (recipe: Recipe) => {
-        return recipe.type === "main";
-    });
-    setMainRecipes(mainRecipes);
-    setCurrentRecipe(mainRecipes[0]);
-  }, []);
-
-  useEffect(() => {
-    let recipes:Recipe[] = [];
-
-    if (groupId === 0)
-      recipes = breakfast_recipes;
-    else if (groupId === 1)
-      recipes = dessert_recipes;
-    else if (groupId === 2)
-      recipes = dinner_recipes;
-    else if (groupId === 3)
-      recipes = gluten_free_recipes;
-    else if (groupId === 4)
-      recipes = side_recipes;
-
-    setRecipes(recipes);
     let mainRecipes = _.filter(recipes, (recipe: Recipe) => {
         return recipe.type === "main";
     });
+    
     setMainRecipes(mainRecipes);
     setCurrentRecipe(mainRecipes[0]);
+
+    if (currentRecipe && currentRecipe.most_similar.length > 0) {
+      setSimilarRecipes(_.filter(recipes, (rcp: Recipe) => {
+        console.log(rcp.id);
+        console.log(currentRecipe ? currentRecipe.most_similar.includes(rcp.id) : false);
+        return currentRecipe ? currentRecipe.most_similar.includes(rcp.id) : false;
+      }));
+      setCertainRecipes(_.filter(similar_recipes, (rcp: Recipe) => {
+        return rcp.type === 'certain';
+      }));
+      setUncertainRecipes(_.filter(similar_recipes, (rcp: Recipe) => {
+        return rcp.type === 'uncertain';
+      }));
+    }
+  }
+
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  useEffect(() => {
+    refresh();
   }, [index])
+
+  useEffect(() => {
+    refresh();
+  }, [directionsAccepted])
   
   return index === -1 ? (
     <Fragment>
@@ -125,17 +163,31 @@ export const HomePage: React.FC = () => {
             <p>
               In this experiment, you will be selecting recipes based on a given scenario and then rating recommendations based on your selection. You can select recipes by clicking on them.
             <br/>
+            <br/>
               Five (5) scenarios will be presented. You should select the recipe you think is the best. There is no right answer, simply pick the one you think looks the best, would be the most delicious, is the easiest to cook - whatever. 
+            <br/>
             <br/>
               Upon selecting a recipe, you will be presented with ten (10) recommended recipes. You can cycle through the recipes by clicking on them. 
             <br/>
+            <br/>
               You will then fill out a survey related to the recommended recipes and the recommendation system as a whole. You will rate these from 1-5 by clicking on the stars. 
             </p>
+            <form>
+              <label>
+                <input 
+                  name="directionsCheckbox"
+                  type="checkbox"
+                  onChange={directionsCheckboxChange}/>
+                  I understand these directions and accept the terms and conditions.
+                  <br/>
+              </label>
+              <input type="submit" value="Submit" disabled={!directionsAccepted} onClick={handleSubmit}/>
+            </form>
           </CardBody>
         </Card>
       </View>
     </Fragment>
-   ) :
+   ) : 
   currentRecipe ? (index === 0 ? (    
     <Fragment>
       <View style={{flex: 1, flexDirection: 'row'}}>
@@ -178,16 +230,66 @@ export const HomePage: React.FC = () => {
     </Fragment>
   ) 
   : index === 1 ?   
-  <Fragment>
-    <View style={{flex: 1, flexDirection: 'row'}}>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <RecipeComp recipe={currentRecipe} />
-      </View>
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <SurveyComp submitSurvey={selectRecipeClick}/>
-      </View>
-    </View>
-  </Fragment> : (<div></div>)) : (<div></div>);  
+    <Fragment>
+      <div> 
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{flex: 1, flexDirection: 'column'}}>
+            <View style={{flex: 3, flexDirection: 'row'}}>
+              <div className='recipe_div'>
+                <RecipeComp recipe={tempCurrentRecipe} />
+              </div>
+            </View>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+              <View style={{flex: 1, flexDirection: 'column'}}>
+                <div id="recipe1" onClick={() => handleClick(similar_recipes[0])}>
+                  <span>1. { similar_recipes[0].title }</span>
+                </div>
+                <div id="recipe2" onClick={() => handleClick(similar_recipes[1])}>
+                  <span>2. { similar_recipes[1].title }</span>
+                </div>
+                <div id="recipe3" onClick={() => handleClick(similar_recipes[2])}>
+                  <span>3. { similar_recipes[2].title }</span>
+                </div>
+                <div id="recipe4" onClick={() => handleClick(similar_recipes[3])}>
+                  <span>4. { similar_recipes[3].title }</span>
+                </div>
+                <div id="recipe5" onClick={() => handleClick(similar_recipes[4])}>
+                  <span>5. { similar_recipes[4].title }</span>
+                </div>
+              </View>
+              <View style={{flex: 1, flexDirection: 'column'}}>
+                <div id="recipe6" onClick={() => handleClick(similar_recipes[5])}>
+                  <span>6. { similar_recipes[5].title }</span>
+                </div>
+                <div id="recipe7" onClick={() => handleClick(similar_recipes[6])}>
+                  <span>7. { similar_recipes[6].title }</span>
+                </div>
+                <div id="recipe8" onClick={() => handleClick(similar_recipes[7])}>
+                  <span>8. { similar_recipes[7].title }</span>
+                </div>
+                <div id="recipe9" onClick={() => handleClick(similar_recipes[8])}>
+                  <span>9. { similar_recipes[8].title }</span>
+                </div>
+                <div id="recipe10" onClick={() => handleClick(similar_recipes[9])}>
+                  <span>10. { similar_recipes[9].title }</span>
+                </div>
+              </View>
+            </View>
+          </View>
+          <View style={{flex: 1, flexDirection: 'column'}}>
+            <div className='main_div'> 
+            <SurveyComp submitSurvey={submitSurvey} recipe={currentRecipe} similar_recipes={allRecipes}/>
+            </div> 
+          </View>
+        </View>
+      </div>
+    </Fragment> 
+  : index === 2 ? 
+    <Fragment>
+      <span>Thank you for participating!</span>
+    </Fragment>
+  : (<div>Current Recipe isn't set. Please contact Carson (385) 244-6611</div>)) 
+  : (<div>Index doesn't equal -1. Please contact Carson (385) 244-6611</div>);  
   
 }
 
